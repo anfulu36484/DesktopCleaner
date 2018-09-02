7db18fb380daa4e9bf10c783ceb16c7183acebfc
anfulu36484 <anfulu36484@gmail.com>
2018-09-02 18:40:24 +0300

    initial commit

---
+++ DesktopCleaner/SettingsInfoForExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopCleaner
+{
+    class SettingsInfo
+    {
+        public string NameOfOutputFolder { get; set; }
+        public List<string> FileTypes { get; set; }
+
+        public SettingsInfo() { }
+
+        public SettingsInfo(string NameOfOutputFolder, List<string> fileTypes)
+        {
+            this.NameOfOutputFolder = NameOfOutputFolder;
+            this.FileTypes = fileTypes;
+        }
+    }
+
+
+}
---
+++ DesktopCleaner/SettingsLoader.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DesktopCleaner
+{
+    class SettingsLoader
+    {
+        public string GetOutputFolderForAnyOtherExtension()
+        {
+            XDocument doc = XDocument.Load("Settings.xml");
+            List<SettingsInfo> listSettingsInfo = new List<SettingsInfo>();
+            string folderName="";
+
+            foreach (XElement SIElement in doc.Element("SettingsInfo").Elements("SettingsForanyAnyOtherExtension"))
+            {
+                folderName= SIElement.Element("NameOfOutputFolder").Value;               
+            }
+            return folderName;
+        }
+
+        public List<SettingsInfo> LoadSettingsForExtensions()
+        {
+            XDocument doc = XDocument.Load("Settings.xml");
+            List<SettingsInfo> listSettingsInfo = new List<SettingsInfo>();
+
+            foreach (XElement SIElement in doc.Element("SettingsInfo").Elements("SettingsForOneTypeOfFiles"))
+            {
+                SettingsInfo SI = new SettingsInfo();
+
+                SI.NameOfOutputFolder = SIElement.Element("NameOfOutputFolder").Value;
+
+                List<string> fileTypes= new List<string>();
+
+                foreach (var n in SIElement.Elements("FileType"))
+                {
+                    fileTypes.Add(n.Value);
+                }
+
+                SI.FileTypes = fileTypes;
+
+                listSettingsInfo.Add(SI);
+            }
+
+            return listSettingsInfo;
+        }
+
+        public List<SettingsInfo> LoadSettingsForKeywords()
+        {
+            XDocument doc = XDocument.Load("Settings.xml");
+            List<SettingsInfo> listSettingsInfo = new List<SettingsInfo>();
+
+            foreach (XElement SIElement in doc.Element("SettingsInfo").Elements("SettingsForKeywords"))
+            {
+                SettingsInfo SI = new SettingsInfo();
+
+                SI.NameOfOutputFolder = SIElement.Element("NameOfOutputFolder").Value;
+
+                List<string> fileTypes = new List<string>();
+
+                foreach (var n in SIElement.Elements("KeyWord"))
+                {
+                    fileTypes.Add(n.Value);
+                }
+
+                SI.FileTypes = fileTypes;
+
+                listSettingsInfo.Add(SI);
+            }
+
+            return listSettingsInfo;
+        }
+
+    }
+}
